a3d234d6dfde4253350c0ae0016eb77ba4ab9831
Sezer <limanlarsezer@gmail.com>
2023-04-04 01:35:05 +0300

    update

---
+++ kodluyoruz.js
@@ -0,0 +1,45 @@
+/* const filmList = (list) =>{
+    list.forEach((series,index) =>{
+        console.log(`${index+1}.${series}`)
+
+    })
+
+}
+filmList(["twd","tw","lc","tsa"]) */
+
+ let baslik = document.querySelector('#baslik');
+
+ //* MOUSE EVENTİ 
+ 
+/*baslik.addEventListener('mouseover', domClick)
+
+
+function domClick(){
+        console.log(this.style.color == "blue" ? this.style.color = "black" : this.style.color = "blue")
+} */
+
+//* KLAVYE EVENTİ
+
+/* baslik.addEventListener("copy",cuttingFunction);
+
+function cuttingFunction(event){
+  console.log("etkinlik tipi: " , event.type)
+  }  */
+
+/*   const selectBox = document.querySelector('#selectColor');
+const button = document.querySelector('#button');
+const form = document.querySelector('.inputText');  
+const cerceve = document.querySelector('.container');
+selectBox.addEventListener('click' ,function(){
+    console.log("selectBox");    
+    })
+form.addEventListener('click',function(){
+    console.log("form");
+    })
+cerceve.addEventListener('click',function(){
+    console.log('cerceve');
+    })
+button.addEventListener('click',function(){
+        console.log('buton');
+        })
+ */
